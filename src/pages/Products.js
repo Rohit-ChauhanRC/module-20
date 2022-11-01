f486e497648436e3b0f8629b1db040e770cfc2cc
ROHIT CHAUHAN <rc672800@gmail.com>
2022-11-01 12:39:03 +0530

    redirect done

---
+++ src/pages/Products.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Products = () => {
+  return (
+    <section>
+      <h1>The Products Page</h1>
+      <ul>
+        <li>
+          <Link to="/products/p1">A Book</Link>
+        </li>
+        <li>
+          <Link to="/products/p2">A Carpet</Link>
+        </li>
+        <Link to="/products/p3"> An Online Course</Link>
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
